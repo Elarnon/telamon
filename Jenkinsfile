979239d2395638c1ab1bd03129026975348175ac
Basile Clement <basile@clement.pm>
2018-07-10 12:59:42 +0200

    Experiment test

---
+++ Jenkinsfile
@@ -0,0 +1,20 @@
+pipeline {
+    agent { label 'ficus' }
+
+    stages {
+        stage('Build') {
+            steps {
+                sh 'mkdir -p output && cp Settings.toml output'
+                dir('output') {
+                    sh 'RUST_LOG=telamon::explorer=warn cargo run --manifest-path ../kernels/Cargo.toml --release --bin search -- --device cuda'
+                }
+            }
+
+            post {
+                success {
+                    archiveArtifacts(artifacts: 'output/*')
+                }
+            }
+        }
+    }
+}
---
+++ kernels/src/bin/search.rs
@@ -0,0 +1,80 @@
+extern crate env_logger;
+extern crate getopts;
+extern crate telamon;
+extern crate telamon_kernels;
+
+use telamon::device;
+use telamon::device::x86;
+use telamon::explorer::config::Config;
+use telamon_kernels::{linalg, Kernel};
+
+#[cfg(feature = "cuda")]
+mod cuda_wrapper {
+    use telamon::device::cuda;
+
+    type CudaContext = cuda::Context;
+
+    pub fn with_cuda_context<F: FnOnce(&mut CudaContext)>(f: F) {
+        f(&mut cuda::Context::new(&cuda::Executor::init()));
+    }
+}
+
+#[cfg(not(feature = "cuda"))]
+mod cuda_wrapper {
+    use telamon::device::x86;
+
+    type CudaContext = x86::Context;
+
+    pub fn with_cuda_context<F: FnOnce(&mut CudaContext)>(_f: F) {
+        panic!("cuda support is not available. Try --device x86.");
+    }
+}
+
+fn with_x86_context<F: FnOnce(&mut x86::Context)>(f: F) {
+    f(&mut x86::Context::new())
+}
+
+fn run<C: device::ArgMap + device::Context>(config: &Config, context: &mut C) {
+    let params = linalg::MatMulP::new(1024, 1024, 1024);
+    linalg::MatMul::<f32>::benchmark(&config, params, 0, context);
+}
+
+enum Device {
+    X86,
+    Cuda,
+}
+
+fn main() {
+    env_logger::init();
+
+    let matches = {
+        let mut opts = getopts::Options::new();
+        opts.optopt("d", "device", "device to use (x86 or cuda)", "[x86 | cuda]");
+        let args: Vec<_> = std::env::args().collect();
+        opts.parse(&args[1..]).unwrap_or_else(|err| {
+            println!("{}", err);
+            std::process::exit(-1);
+        })
+    };
+    let device = if let Some(device_str) = matches.opt_str("d") {
+        match device_str.as_ref() {
+            "x86" => Device::X86,
+            "cuda" => Device::Cuda,
+            device_str => {
+                println!(
+                    "Invalid device {}. Valid devices are: x86 and cuda.",
+                    device_str
+                );
+                std::process::exit(-1);
+            }
+        }
+    } else {
+        Device::Cuda
+    };
+
+    let config = Config::read_from_file();
+    match device {
+        Device::X86 => with_x86_context(|context| run(&config, context)),
+        Device::Cuda => cuda_wrapper::with_cuda_context(|context| run(&config, context)),
+    }
+}
